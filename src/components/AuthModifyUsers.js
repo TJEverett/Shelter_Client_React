45d12966f73581b165fc611d01f7183150cfa903
Tristen Everett <spock214@gmail.com>
2023-06-16 15:26:22 -0700

    update to use errorModal in place of an alert for displaying error messages

--- src/components/AuthModifyUsers.js
+++ src/components/AuthModifyUsers.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ApiAuthCall, errorClear } from "../actions/index";
 import AuthForm from "./AuthForm";
+import ErrorModal from "./ErrorModal";
 
 function AuthModifyUsers(){
   //Styles Object
@@ -16,6 +17,11 @@ function AuthModifyUsers(){
       borderRadius: "50px",
       margin: "20px",
       padding: "10px"
+    },
+    center: {
+      display: "flex",
+      justifyContent: "center",
+      alignContent: "center"
     }
   };
 
@@ -42,19 +48,36 @@ function AuthModifyUsers(){
     let action = ApiAuthCall("delete", attempt);
     dispatch(action);
   }
+  function ClearError(){
+    const action = errorClear();
+    dispatch(action);
+  }
 
   //Render Logic
-  function RenderAlert(){
-    if(errorMessage !== null){
-      alert(errorMessage);
-      const action = errorClear();
-      dispatch(action);
+  let modalBlock = <div />
+  let modalContents = [];
+  let modalMode = true;
+
+  if(errorMessage !== null){
+    if(typeof errorMessage === "string"){
+      modalContents.push(<h2 key={"h2Tag1"}>Server Message:</h2>);
+      modalContents.push(<h3 key={"h3Tag1"}>{errorMessage}</h3>);
+      modalContents.push(<div style={styles.center} key={"clearButton1"}><button onClick={ClearError}>Clear</button></div>);
+      modalMode = false;
+    }else{
+      modalContents.push(<h2 key={"h2Tag2"}>ERROR: {errorMessage.message}</h2>);
+      modalContents.push(<div style={styles.center} key={"clearButton2"}><button onClick={ClearError}>Clear</button></div>);
     }
+    modalBlock = <ErrorModal show={true} handleClose={ClearError} errorMode={modalMode} >
+      <div style={{...styles.center, ...{flexDirection: "column"}}}>
+        {modalContents}
+      </div>
+    </ErrorModal>
   }
 
   return(
     <React.Fragment>
-      {RenderAlert()}
+      {modalBlock}
       <div style={styles.grid}>
         <div />
         <div style={styles.borderBubble}>
