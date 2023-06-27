14e1aa7f4ccdbdadfc968955e95deb0fe6e467a6
Tristen Everett <spock214@gmail.com>
2023-06-27 15:35:45 -0700

    remove temp values and add keys

--- src/components/App.js
+++ src/components/App.js
@@ -8,88 +8,6 @@ import NavBar from "./NavBar";
 import SearchResult from "./SearchResult";
 
 function App() {
-  //Temp Values
-  // const authStatus = null;
-  // const authStatus = "1hr 52min";
-  const animalArray = [
-    {
-      id: "c1",
-      name: "Taz",
-      weightkilo: 5.5,
-      isfemale: false,
-      birthday: "2017-09-08T19:01:55",
-      coloring: "black",
-      description: "indoor / outdoor"
-    },
-    {
-      id: "c2",
-      name: "Phantom",
-      weightkilo: 2.5,
-      isfemale: true,
-      birthday: "2022-09-08T19:01:55",
-      coloring: "black",
-      description: "indoor"
-    },
-    {
-      id: "c3",
-      name: "Sasha",
-      weightkilo: 5.1,
-      isfemale: true,
-      birthday: "2017-09-08T19:01:55",
-      coloring: "grey striped",
-      description: "indoor"
-    },
-    {
-      id: "c4",
-      name: "Tigger",
-      weightkilo: 4.3,
-      isfemale: false,
-      birthday: "2022-09-08T19:01:55",
-      coloring: "grey striped",
-      description: "indoor"
-    },
-    {
-      id: "d1",
-      name: "Ceres",
-      weightkilo: 99.3,
-      isfemale: false,
-      birthday: "2017-09-08T19:01:55",
-      coloring: "blonde",
-      description: "known as Littles",
-      breed: "English Mastiff"
-    },
-    {
-      id: "d2",
-      name: "Cali",
-      weightkilo: 23.4,
-      isfemale: true,
-      birthday: "2022-09-08T19:01:55",
-      coloring: "black",
-      description: "meth lab",
-      breed: "Mutt"
-    },
-    {
-      id: "d3",
-      name: "Elsa",
-      weightkilo: 24.3,
-      isfemale: true,
-      birthday: "2017-09-08T19:01:55",
-      coloring: "black with brown highlights",
-      description: "allergic to food",
-      breed: "Doberman"
-    },
-    {
-      id: "d4",
-      name: "Lowkey",
-      weightkilo: 27.5,
-      isfemale: false,
-      birthday: "2022-09-08T19:01:55",
-      coloring: "black / blonde",
-      description: "tries to eat playmates",
-      breed: "German Shepard"
-    }
-  ] //Will be replaced by API
-
   function CheckAuth(newRoute){
     const auth = useSelector((state) => state.auth.token);
     if(auth === null){
@@ -105,44 +23,46 @@ function App() {
   return(
     <BrowserRouter>
       <NavBar />
-      <div style={{height: "70px", width: "100vh"}}>
+      <div style={{height: "70px", width: "100vw"}}>
         <p>NavBar spacer</p>
       </div>
       <Switch>
         <Route exact path="/cats">
-          <SearchResult animalType="cat" />
+          <SearchResult animalType="cat" key="dogSearch" />
         </Route>
         <Route path="/cats/edit">
           {CheckAuth("/cats")}
-          <AnimalModify editMode={true} species="cat" animal={animalArray[0]} />
+          <AnimalModify editMode={true} species="cat" key="catEdit" />
         </Route>
         <Route path="/cats/new">
           {CheckAuth("/cats")}
-          <AnimalModify editMode={false} species="cat" />
+          <AnimalModify editMode={false} species="cat" key="catCreate" />
         </Route>
 
         <Route exact path="/dogs">
-          <SearchResult animalType="dog" />
+          <SearchResult animalType="dog" key="dogSearch" />
         </Route>
         <Route path="/dogs/edit">
           {CheckAuth("/dogs")}
-          <AnimalModify editMode={true} species="dog" animal={animalArray[4]} />
+          <AnimalModify editMode={true} species="dog" key="dogEdit" />
         </Route>
         <Route path="/dogs/new">
           {CheckAuth("/dogs")}
-          <AnimalModify editMode={false} species="dog" />
+          <AnimalModify editMode={false} species="dog" key="dogCreate" />
         </Route>
 
         <Route exact path="/auth">
-          <AuthSignIn />
+          <AuthSignIn key="auth" />
         </Route>
         <Route path="/auth/other">
           {CheckAuth("/auth")}
-          <AuthModifyUsers />
+          <AuthModifyUsers key="authModify" />
         </Route>
 
         <Route path="/">
-          <h2 style={{height: "300vh"}}>default path</h2>
+          <div style={{ height: "100vh", display: "flex", justifyContent: "center" }}>
+            <h1>Animal Shelter Web App</h1>
+          </div>
         </Route>
       </Switch>
     </BrowserRouter>
